a059df9d04d034e172dc6b4b2802174a1129ffce
Rich Boyce <122512687+rboyce212@users.noreply.github.com>
2023-11-23 22:51:10 -0500

    some style changes for buttons and headers

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -3,7 +3,7 @@ import "../Style/home.css";
 export default function Home() {
   return (
     <div className="Home">
-      <h2>Welcome to the Wedding Invites Manager 1.0</h2>
+      <h2>Welcome to the Wedding Invites Manager.</h2>
       <h3>Let me help you manage the invitations for your wedding guests!</h3>
     </div>
   );
